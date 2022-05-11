607424f60e4305dd892b80a63dd11774492c25bd
Martha Moreno <88118070+martha-moreno@users.noreply.github.com>
2022-05-11 10:45:31 -0600

    test failure

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,15 @@
 const hello = require("./hello");
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try{ 
+
+
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World");
+    expect(hello.hello()).toEqual("Hello World!");
   });
 });
+}catch (error) {
+  core.setFailed(error.message);
+}
